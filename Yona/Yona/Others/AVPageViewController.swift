ad66c129ebec733761e4637625892bc20d8a7caf
Ben Smith <benjaminsmith1981@gmail.com>
2016-04-20 18:12:33 +0200

    removed unused code

--- Yona/Yona/Others/AVPageViewController.swift
+++ Yona/Yona/Others/AVPageViewController.swift
@@ -25,7 +25,6 @@ class AVPageViewController: UIPageViewController, UIPageViewControllerDataSource
         self.setupViewControllerIndex(contentViews)
         self.view.frame = viewControllerFrameRect
         let viewController = NSArray(object: self.viewController(atIndex: self.presentingIndex))
-//        self.setViewControllers(viewController as? [UIViewController], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
         self.setViewControllers(
             viewController as? [UIViewController],
             direction: UIPageViewControllerNavigationDirection.Forward,
