2af2b7ee8a97f1cb4cf006689ed2fc191d6a9cab
Alessio Roberto <aroberto@mobiquityinc.com>
2016-04-27 11:12:27 +0200

    Fixed HockeyApp commands order and add update on launch check

--- Yona/Yona/AppDelegate.swift
+++ Yona/Yona/AppDelegate.swift
@@ -63,8 +63,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             BITHockeyManager.sharedHockeyManager().configureWithIdentifier(secretKey)
             // Do some additional configuration if needed here
             BITHockeyManager.sharedHockeyManager().testIdentifier()
-            BITHockeyManager.sharedHockeyManager().authenticator.authenticateInstallation()
             BITHockeyManager.sharedHockeyManager().startManager()
+            BITHockeyManager.sharedHockeyManager().authenticator.authenticateInstallation()
+            #if DEBUG
+                BITHockeyManager.sharedHockeyManager().updateManager.checkForUpdateOnLaunch = false
+            #else
+                BITHockeyManager.sharedHockeyManager().updateManager.checkForUpdateOnLaunch = true
+            #endif
         }
     }
     
