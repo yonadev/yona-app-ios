5f050d14431f5fb551ab5c4241c8700014680ae7
Alessio Roberto <aroberto@mobiquityinc.com>
2016-05-03 14:38:48 +0200

    Try to fix issue with pods

--- Yona/Yona/Others/Yona-Bridging-Header.h
+++ Yona/Yona/Others/Yona-Bridging-Header.h
@@ -2,8 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-<<<<<<< HEAD
 #import "MacroAccessor.h"
-=======
 #import "MBProgressHUD.h"
->>>>>>> develop
