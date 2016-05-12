8ef16ae7ddd57ed6ed236086545cd1f9e4989eb0
chandanvarma009 <chandanvarma009@gmail.com>
2016-05-12 15:59:00 +0530

    address list displayed as ABPeoplePickerNavigationController

--- Yona/Yona/Friends/AddFriendsViewController.swift
+++ Yona/Yona/Friends/AddFriendsViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import AddressBook
+import AddressBookUI
 
-class AddFriendsViewController: UIViewController, UIScrollViewDelegate {
+class AddFriendsViewController: UIViewController, UIScrollViewDelegate, UINavigationControllerDelegate, ABPeoplePickerNavigationControllerDelegate {
     
     @IBOutlet var gradientView: GradientView!
     @IBOutlet var manualTabView: UIView!
@@ -23,6 +24,8 @@ class AddFriendsViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet var inviteFriendButton: UIButton!
     @IBOutlet var screenTitle: UILabel!
     var addressBook: ABAddressBookRef?
+    var people = ABPeoplePickerNavigationController()
+    
     
     
     // MARK: - View
@@ -90,10 +93,84 @@ class AddFriendsViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func createAddressBook(){
+        
         var error: Unmanaged<CFError>?
         addressBook = ABAddressBookCreateWithOptions(nil, &error).takeRetainedValue()
         
     }
+    
+    func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController, didSelectPerson person: ABRecord) {
+        peoplePicker.dismissViewControllerAnimated(true, completion: nil);
+        let index = 0 as CFIndex;
+        
+        //Get Name
+        var firstName:String?;
+        let firstNameObj = ABRecordCopyValue(person, kABPersonFirstNameProperty);
+        
+        if(firstNameObj != nil) {
+            firstName = firstNameObj.takeRetainedValue() as? String;
+        } else {
+            firstName = "";
+        }
+        firstnameTextfield.text = firstName
+        
+        var lastName:String?;
+        let lastNameObj = ABRecordCopyValue(person, kABPersonLastNameProperty);
+        if(lastNameObj != nil) {
+            lastName = lastNameObj.takeRetainedValue() as? String;
+        } else {
+            lastName = "";
+        }
+        lastnameTextfield.text = lastName
+        
+        //Get phone number
+        var phoneNumber:String?;
+        let unmanagedPhones:Unmanaged? = ABRecordCopyValue(person, kABPersonPhoneProperty);
+        if(unmanagedPhones != nil) {
+            let phoneNumbers = unmanagedPhones?.takeRetainedValue();
+            if(ABMultiValueGetCount(phoneNumbers) > 0) {
+                phoneNumber = ABMultiValueCopyValueAtIndex(phoneNumbers, index).takeRetainedValue() as? String;
+                phoneNumber = "";
+            } else {
+                phoneNumber = "";
+            }
+            mobileTextfield.text = phoneNumber;
+        }
+        
+        //Get email
+        let emailProperty: ABMultiValueRef = ABRecordCopyValue(person, kABPersonEmailProperty).takeRetainedValue() as ABMultiValueRef
+        let allEmailIDs: NSArray = ABMultiValueCopyArrayOfAllValues(emailProperty).takeUnretainedValue() as NSArray
+        for email in allEmailIDs {
+            let emailID = email as! String
+            print ("contactEmail : \(emailID) :=>")
+            emailTextfield.text = emailID
+        }
+    }
+    
+    //    func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController, didSelectPerson person: ABRecordRef, property: ABPropertyID, identifier: ABMultiValueIdentifier) {
+    //        let multiValue: ABMultiValueRef = ABRecordCopyValue(person, property).takeRetainedValue()
+    //        let index = ABMultiValueGetIndexForIdentifier(multiValue, identifier)
+    //        let email = ABMultiValueCopyValueAtIndex(multiValue, index).takeRetainedValue() as! String
+    //
+    //        print("email = \(email)")
+    //    }
+    
+    func peoplePickerNavigationControllerDidCancel(peoplePicker: ABPeoplePickerNavigationController!) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController, shouldContinueAfterSelectingPerson person: ABRecord, property: ABPropertyID, identifier: ABMultiValueIdentifier) -> Bool {
+        let multiValue: ABMultiValueRef = ABRecordCopyValue(person, property).takeRetainedValue()
+        let index = ABMultiValueGetIndexForIdentifier(multiValue, identifier)
+        let email = ABMultiValueCopyValueAtIndex(multiValue, index).takeRetainedValue() as! String
+        
+        print("email = \(email)")
+        
+        peoplePicker.dismissViewControllerAnimated(true, completion: nil)
+        
+        return false
+    }
+    
     
     // Go Back To Previous VC
     @IBAction func back(sender: AnyObject) {
@@ -108,7 +185,12 @@ class AddFriendsViewController: UIViewController, UIScrollViewDelegate {
         switch ABAddressBookGetAuthorizationStatus(){
         case .Authorized:
             print("Already authorized")
+            
             createAddressBook()
+            people.peoplePickerDelegate = self
+            dispatch_async(dispatch_get_main_queue(), {
+                self.presentViewController(self.people, animated: true, completion: nil)
+            })
             /* Access the address book */
         case .Denied:
             print("Denied access to address book")
@@ -120,10 +202,11 @@ class AddFriendsViewController: UIViewController, UIScrollViewDelegate {
                                                          {(granted: Bool, error: CFError!) in
                                                             
                                                             if granted{
-                                                                if let people = ABAddressBookCopyArrayOfAllPeople(self.addressBook)?.takeRetainedValue() as? NSArray {
-                                                                    // now do something with the array of people
-                                                                }
                                                                 print("Access granted")
+                                                                self.people.peoplePickerDelegate = self
+                                                                dispatch_async(dispatch_get_main_queue(), {
+                                                                    self.presentViewController(self.people, animated: true, completion: nil)
+                                                                })
                                                             } else {
                                                                 print("Access not granted")
                                                             }
@@ -158,6 +241,7 @@ extension AddFriendsViewController {
         addressBookTabBottomBorder.hidden = false
         
         addressbookAccess()
+        
     }
 }
 
