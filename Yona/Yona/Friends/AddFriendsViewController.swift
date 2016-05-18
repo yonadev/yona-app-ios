2331ccebb79384364cd8fdfcc7c5fa22fae58c76
chandanvarma009 <chandanvarma009@gmail.com>
2016-05-18 18:39:51 +0530

    Add friend mobile number will now contains countrycode

--- Yona/Yona/Friends/AddFriendsViewController.swift
+++ Yona/Yona/Friends/AddFriendsViewController.swift
@@ -25,8 +25,8 @@ class AddFriendsViewController: UIViewController, UIScrollViewDelegate, UINaviga
     @IBOutlet var screenTitle: UILabel!
     var addressBook: ABAddressBookRef?
     var people = ABPeoplePickerNavigationController()
-    
-    
+    var previousRange: NSRange!
+    private let nederlandPhonePrefix = "+31 (0) "
     
     // MARK: - View
     override func viewDidLoad() {
@@ -37,6 +37,10 @@ class AddFriendsViewController: UIViewController, UIScrollViewDelegate, UINaviga
     
     // MARK: - private functions
     private func setupUI() {
+        if var label = previousRange {
+            label.length = 1
+        }
+        
         gradientView.colors = [UIColor.yiMidBlueColor(), UIColor.yiMidBlueColor()]
         
         //Nav bar Back button.
@@ -170,103 +174,151 @@ class AddFriendsViewController: UIViewController, UIScrollViewDelegate, UINaviga
             self.displayAlertMessage("", alertDescription:
                 NSLocalizedString("enter-number-validation", comment: ""))
             
-        } else {
+            } else {
+            var number = ""
+            if let mobilenum = mobileTextfield.text {
+                number = (nederlandPhonePrefix) + mobilenum
+                
+                let trimmedWhiteSpaceString = number.removeWhitespace()
+                let trimmedString = trimmedWhiteSpaceString.removeBrackets()
+                
+                if trimmedString.validateMobileNumber() == false {
+                    self.displayAlertMessage("", alertDescription:
+                        NSLocalizedString("enter-number-validation", comment: ""))
+                    
+                }
             
-            let postBuddyBody: [String:AnyObject] = [
-                postBuddyBodyKeys.sendingStatus.rawValue: buddyRequestStatus.REQUESTED.rawValue,
-                postBuddyBodyKeys.receivingStatus.rawValue: buddyRequestStatus.REQUESTED.rawValue,
-                postBuddyBodyKeys.message.rawValue : "Hi there, would you want to become my buddy?",
-                postBuddyBodyKeys.embedded.rawValue: [
-                    postBuddyBodyKeys.yonaUser.rawValue: [  //this is the details of the person you are adding
-                        addUserKeys.emailAddress.rawValue: emailTextfield.text ?? "",
-                        addUserKeys.firstNameKey.rawValue: firstnameTextfield.text ?? "",
-                        addUserKeys.lastNameKeys.rawValue: lastnameTextfield.text ?? "",
-                        addUserKeys.mobileNumberKeys.rawValue: mobileTextfield.text ?? ""  //this is the number of the person you are adding as a buddy
+            
+                let postBuddyBody: [String:AnyObject] = [
+                    postBuddyBodyKeys.sendingStatus.rawValue: buddyRequestStatus.REQUESTED.rawValue,
+                    postBuddyBodyKeys.receivingStatus.rawValue: buddyRequestStatus.REQUESTED.rawValue,
+                    postBuddyBodyKeys.message.rawValue : "Hi there, would you want to become my buddy?",
+                    postBuddyBodyKeys.embedded.rawValue: [
+                        postBuddyBodyKeys.yonaUser.rawValue: [  //this is the details of the person you are adding
+                            addUserKeys.emailAddress.rawValue: emailTextfield.text ?? "",
+                            addUserKeys.firstNameKey.rawValue: firstnameTextfield.text ?? "",
+                            addUserKeys.lastNameKeys.rawValue: lastnameTextfield.text ?? "",
+                            addUserKeys.mobileNumberKeys.rawValue: trimmedString //this is the number of the person you are adding as a buddy
+                        ]
                     ]
                 ]
-            ]
-            
-            BuddyRequestManager.sharedInstance.requestNewbuddy(postBuddyBody, onCompletion: { (success, message, code, buddy, buddies) in
                 
-                if success {
-                    self.navigationController?.popViewControllerAnimated(true)
-                } else {
-                    if let message = message {
-                        self.displayAlertMessage("", alertDescription:message)
+                BuddyRequestManager.sharedInstance.requestNewbuddy(postBuddyBody, onCompletion: { (success, message, code, buddy, buddies) in
+                    
+                    if success {
+                        self.navigationController?.popViewControllerAnimated(true)
+                    } else {
+                        if let message = message {
+                            self.displayAlertMessage("", alertDescription:message)
+                        }
                     }
-                }
-            })
-        }
-    }
-    
-    func addressbookAccess() {
-        switch ABAddressBookGetAuthorizationStatus(){
-        case .Authorized:
-            print("Already authorized")
-            
-            createAddressBook()
-            people.peoplePickerDelegate = self
-            dispatch_async(dispatch_get_main_queue(), {
-                self.presentViewController(self.people, animated: true, completion: nil)
-            })
-            /* Access the address book */
-        case .Denied:
-            print("Denied access to address book")
-            
-        case .NotDetermined:
-            createAddressBook()
-            if let theBook: ABAddressBookRef = addressBook{
-                ABAddressBookRequestAccessWithCompletion(theBook,
-                                                         {(granted: Bool, error: CFError!) in
-                                                            
-                                                            if granted{
-                                                                print("Access granted")
-                                                                self.people.peoplePickerDelegate = self
-                                                                dispatch_async(dispatch_get_main_queue(), {
-                                                                    self.presentViewController(self.people, animated: true, completion: nil)
-                                                                })
-                                                            } else {
-                                                                print("Access not granted")
-                                                            }
-                                                            
                 })
             }
-            
-        case .Restricted:
-            print("Access restricted")
-            
-        default:
-            print("Other Problem")
+        }
+        }
+        
+        func addressbookAccess() {
+            switch ABAddressBookGetAuthorizationStatus(){
+            case .Authorized:
+                print("Already authorized")
+                
+                createAddressBook()
+                people.peoplePickerDelegate = self
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.presentViewController(self.people, animated: true, completion: nil)
+                })
+                /* Access the address book */
+            case .Denied:
+                print("Denied access to address book")
+                
+            case .NotDetermined:
+                createAddressBook()
+                if let theBook: ABAddressBookRef = addressBook{
+                    ABAddressBookRequestAccessWithCompletion(theBook,
+                                                             {(granted: Bool, error: CFError!) in
+                                                                
+                                                                if granted{
+                                                                    print("Access granted")
+                                                                    self.people.peoplePickerDelegate = self
+                                                                    dispatch_async(dispatch_get_main_queue(), {
+                                                                        self.presentViewController(self.people, animated: true, completion: nil)
+                                                                    })
+                                                                } else {
+                                                                    print("Access not granted")
+                                                                }
+                                                                
+                    })
+                }
+                
+            case .Restricted:
+                print("Access restricted")
+                
+            default:
+                print("Other Problem")
+            }
         }
     }
-}
-
-
-// MARK: Touch Event of Custom Segment
-extension AddFriendsViewController {
     
-    @IBAction func ManualTabAction(sender: AnyObject) {
-        addressBookTabView.alpha = 0.5
-        addressBookTabBottomBorder.hidden = true
-        manualTabView.alpha = 1.0
-        manualTabBottomBorder.hidden = false
+    
+    // MARK: Touch Event of Custom Segment
+    extension AddFriendsViewController {
+        
+        @IBAction func ManualTabAction(sender: AnyObject) {
+            addressBookTabView.alpha = 0.5
+            addressBookTabBottomBorder.hidden = true
+            manualTabView.alpha = 1.0
+            manualTabBottomBorder.hidden = false
+        }
+        
+        @IBAction func AddressBookTabAction(sender: AnyObject) {
+            manualTabView.alpha = 0.5
+            manualTabBottomBorder.hidden = true
+            addressBookTabView.alpha = 1.0
+            addressBookTabBottomBorder.hidden = false
+            
+            addressbookAccess()
+            
+        }
     }
     
-    @IBAction func AddressBookTabAction(sender: AnyObject) {
-        manualTabView.alpha = 0.5
-        manualTabBottomBorder.hidden = true
-        addressBookTabView.alpha = 1.0
-        addressBookTabBottomBorder.hidden = false
+    
+    extension AddFriendsViewController: UITextFieldDelegate {
         
-        addressbookAccess()
+        func textFieldDidBeginEditing(textField: UITextField) {
+            if textField == mobileTextfield {
+                IQKeyboardManager.sharedManager().enableAutoToolbar = true
+            } else {
+                IQKeyboardManager.sharedManager().enableAutoToolbar = false
+            }
+        }
         
+        func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+            if (textField == mobileTextfield) {
+                if ((previousRange?.location >= range.location) ) {
+                    if (textField.text?.utf16.count ?? 0) + string.utf16.count - range.length == YonaConstants.mobilePhoneSpace.mobileFirstSpace || (textField.text?.utf16.count ?? 0) + string.utf16.count - range.length == YonaConstants.mobilePhoneSpace.mobileMiddleSpace {
+                        textField.text = String(textField.text!.characters.dropLast())
+                        textField.text = String(textField.text!.characters.dropLast())
+                    }
+                } else  {
+                    if (textField.text?.utf16.count ?? 0) + string.utf16.count - range.length ==  YonaConstants.mobilePhoneSpace.mobileFirstSpace || (textField.text?.utf16.count ?? 0) + string.utf16.count - range.length == YonaConstants.mobilePhoneSpace.mobileMiddleSpace {
+                        let space = " "
+                        
+                        textField.text = "\(textField.text!) \(space)"
+                    }
+                }
+                previousRange = range
+                
+                return (textField.text?.utf16.count ?? 0) + string.utf16.count - range.length <= YonaConstants.mobilePhoneSpace.mobileLastSpace
+            }
+            return true
+        }
     }
+    
+    private extension Selector {
+        static let dismissKeyboard = #selector(AddFriendsViewController.dismissKeyboard)
+        static let back = #selector(AddFriendsViewController.back(_:))
 }
 
-private extension Selector {
-    static let dismissKeyboard = #selector(AddFriendsViewController.dismissKeyboard)
-    static let back = #selector(AddFriendsViewController.back(_:))
-}
 
 extension UITextField {
     class func connectFields(fields:[UITextField]) -> Void {
@@ -281,6 +333,5 @@ extension UITextField {
         last.addTarget(last, action: #selector(UIResponder.resignFirstResponder), forControlEvents: .EditingDidEndOnExit)
     }
 }
-
 
 
