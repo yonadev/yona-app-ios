bd4e65b12a4f17776af1fa2e1c8fc48e74c932d4
Ben Smith <benjaminsmith1981@gmail.com>
2016-06-08 10:49:53 +0200

    set login screen on resign active and logged in to false

--- Yona/Yona/AppDelegate.swift
+++ Yona/Yona/AppDelegate.swift
@@ -39,6 +39,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        setViewControllerToDisplay(ViewControllerTypeString.login,key: YonaConstants.nsUserDefaultsKeys.screenToDisplay)
+        NSUserDefaults.standardUserDefaults().setBool(false, forKey: YonaConstants.nsUserDefaultsKeys.isLoggedIn)
     }
     
     func applicationDidEnterBackground(application: UIApplication) {
@@ -55,6 +57,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        setViewControllerToDisplay(ViewControllerTypeString.login,key: YonaConstants.nsUserDefaultsKeys.screenToDisplay)
+        NSUserDefaults.standardUserDefaults().setBool(false, forKey: YonaConstants.nsUserDefaultsKeys.isLoggedIn)
     }
     
     func applicationWillTerminate(application: UIApplication) {
