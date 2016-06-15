8b3d0d0da97e19c668212b2dd1f8b81e15036788
Anders Liebl <aliebl@mobiquityinc.com>
2016-06-15 16:19:47 +0200

    Profile display and edit
    Badges are still missing

---
+++ Yona/Yona/Profile/ProfileDisplayTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileDisplayTableViewCell.swift
+//  Yona
+//
+//  Created by Anders Liebl on 13/06/2016.
+//  Copyright © 2016 Yona. All rights reserved.
+//
+
+import Foundation
---
+++ Yona/Yona/Profile/ProfileDisplayTopTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileDisplayTopTableViewCell.swift
+//  Yona
+//
+//  Created by Anders Liebl on 15/06/2016.
+//  Copyright © 2016 Yona. All rights reserved.
+//
+
+import Foundation
+
+class ProfileDisplayTopTableViewCell: UITableViewCell {
+
+}
