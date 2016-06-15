c0b215f3c2e031967acbef2742c89137c1bdf998
Ben Smith <benjaminsmith1981@gmail.com>
2016-06-15 16:52:59 +0200

    removed duplicate keyboard methods

--- Yona/Yona/Others/SMSValidPasscodeLogin/SMSValidation/PinResetValidationVC.swift
+++ Yona/Yona/Others/SMSValidPasscodeLogin/SMSValidation/PinResetValidationVC.swift
@@ -52,60 +52,28 @@ final class PinResetValidationVC: ValidationMasterView {
 extension PinResetValidationVC: CodeInputViewDelegate {
     
     func codeInputView(codeInputView: CodeInputView, didFinishWithCode code: String) {
-        //the app becomes blocked if the user enters their passcode incorrectly 5 times, in this case we must verify the pin they enter
-//        if NSUserDefaults.standardUserDefaults().boolForKey(YonaConstants.nsUserDefaultsKeys.isBlocked) {
-            self.codeInputView.userInteractionEnabled = true
-            let body = ["code": code]
-            Loader.Show()
-            PinResetRequestManager.sharedInstance.pinResetVerify(body, onCompletion: { (success, nil, message, code) in
-                Loader.Hide()
-                if success {
-//                    self.pinResetButton.hidden = false
-                    //pin verify succeeded, unblock app
-                    NSUserDefaults.standardUserDefaults().setBool(false, forKey: YonaConstants.nsUserDefaultsKeys.isBlocked)
-                    //clear pincode when reset is verified
-                    PinResetRequestManager.sharedInstance.pinResetClear({ (success, nil, message, code) in
-                        //Now send user back to pinreset screen, let them enter pincode and password again
-                        self.codeInputView.resignFirstResponder()
-                        //Update flag
-                        setViewControllerToDisplay(ViewControllerTypeString.passcode, key: YonaConstants.nsUserDefaultsKeys.screenToDisplay)
-                        self.performSegueWithIdentifier(R.segue.pinResetValidationVC.transToSetPincode, sender: self)
-                        self.codeInputView.clear()
-                    })
-                } else {//pin reset verify code is wrong
-                    self.checkCodeMessageShowAlert(message, serverMessageCode: code, codeInputView: codeInputView)
-                    NSUserDefaults.standardUserDefaults().setBool(true, forKey: YonaConstants.nsUserDefaultsKeys.isBlocked)
+        self.codeInputView.userInteractionEnabled = true
+        let body = ["code": code]
+        Loader.Show()
+        PinResetRequestManager.sharedInstance.pinResetVerify(body, onCompletion: { (success, nil, message, code) in
+            Loader.Hide()
+            if success {
+                //pin verify succeeded, unblock app
+                NSUserDefaults.standardUserDefaults().setBool(false, forKey: YonaConstants.nsUserDefaultsKeys.isBlocked)
+                //clear pincode when reset is verified
+                PinResetRequestManager.sharedInstance.pinResetClear({ (success, nil, message, code) in
+                    //Now send user back to pinreset screen, let them enter pincode and password again
+                    self.codeInputView.resignFirstResponder()
+                    //Update flag
+                    setViewControllerToDisplay(ViewControllerTypeString.passcode, key: YonaConstants.nsUserDefaultsKeys.screenToDisplay)
+                    self.performSegueWithIdentifier(R.segue.pinResetValidationVC.transToSetPincode, sender: self)
                     self.codeInputView.clear()
-                }
-            })
-        }
-//    }
-
-}
-
-
-extension PinResetValidationVC: KeyboardProtocol {
-    func keyboardWasShown (notification: NSNotification) {
-        
-        if let activeField = self.codeView, keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
-            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
-            self.scrollView.contentInset = contentInsets
-            self.scrollView.scrollIndicatorInsets = contentInsets
-            var aRect = self.scrollView.bounds
-            aRect.size.height -= keyboardSize.size.height
-            if (!CGRectContainsPoint(aRect, activeField.frame.origin)) {
-                var frameToScrollTo = activeField.frame
-                frameToScrollTo.size.height += 30
-                self.scrollView.scrollRectToVisible(frameToScrollTo, animated: true)
+                })
+            } else {//pin reset verify code is wrong
+                self.checkCodeMessageShowAlert(message, serverMessageCode: code, codeInputView: codeInputView)
+                NSUserDefaults.standardUserDefaults().setBool(true, forKey: YonaConstants.nsUserDefaultsKeys.isBlocked)
+                self.codeInputView.clear()
             }
-        }
-    }
-    
-    func keyboardWillBeHidden(notification: NSNotification) {
-        let contentInsets = UIEdgeInsetsZero
-        self.scrollView.contentInset = contentInsets
-        self.scrollView.scrollIndicatorInsets = contentInsets
-        
+        })
     }
 }
-
