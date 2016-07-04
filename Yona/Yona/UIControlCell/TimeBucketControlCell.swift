3c9cb8f94af8405cdd07bebb0e2866a933f87d90
Ben Smith <benjaminsmith1981@gmail.com>
2016-07-04 21:45:58 +0200

    Added label buttons and minutes left

--- Yona/Yona/UIControlCell/TimeBucketControlCell.swift
+++ Yona/Yona/UIControlCell/TimeBucketControlCell.swift
@@ -12,11 +12,13 @@ import UIKit
 class TimeBucketControlCell : UITableViewCell {
     
     @IBOutlet weak var goalType: UILabel!
-    @IBOutlet weak var minutes: UILabel!
+    @IBOutlet weak var minutesTitle: UILabel!
     @IBOutlet weak var goalMessage: UILabel!
     @IBOutlet weak var minsView: UIView!
     @IBOutlet weak var backgroundMinsView: UIView!
-    @IBOutlet weak var startMinutes: UILabel!
+    
+    @IBOutlet weak var zeroMinutes: UILabel!
+    @IBOutlet weak var minutesBeyondGoal: UILabel!
     @IBOutlet weak var endMinutes: UILabel!
     
     @IBOutlet weak var horizontalSpaceingConstraint: NSLayoutConstraint!
@@ -26,41 +28,79 @@ class TimeBucketControlCell : UITableViewCell {
     }
     
     func setUpView(activityGoal : ActivitiesGoal) {
-    
-        let neg = activityGoal.totalMinutesBeyondGoal
-        let positive = activityGoal.totalActivityDurationMinutes - activityGoal.totalMinutesBeyondGoal
+
+        let neg = 0//activityGoal.totalMinutesBeyondGoal
+        let positive = 30//activityGoal.totalActivityDurationMinutes - activityGoal.totalMinutesBeyondGoal
         
         let totalMinutes = neg + positive
-        let pxPrMinute = backgroundMinsView.frame.size.width / CGFloat(totalMinutes)
         
-        let negattveView = UIView(frame: CGRectMake(CGFloat(neg)*pxPrMinute,0,0,backgroundMinsView.frame.size.height))
-        negattveView.backgroundColor = UIColor.redColor()
-        negattveView.alpha = 0
+        var pxPrMinute : CGFloat = 0.0 //number of pixels per minute
+        if totalMinutes > 0 {
+            pxPrMinute = backgroundMinsView.frame.size.width / CGFloat(totalMinutes)
+        }
         
-        let positiveView = UIView(frame: CGRectMake(CGFloat(neg)*pxPrMinute,0,CGFloat(positive)*pxPrMinute,backgroundMinsView.frame.size.height))
+        let negativeView = UIView(frame: CGRectMake(CGFloat(neg) * pxPrMinute, 0, 0, backgroundMinsView.frame.size.height))
+        negativeView.backgroundColor = UIColor.redColor()
+        negativeView.alpha = 0
+        
+        let positiveView = UIView(frame: CGRectMake(CGFloat(neg) * pxPrMinute, 0, CGFloat(positive) * pxPrMinute, backgroundMinsView.frame.size.height))
         positiveView.backgroundColor = UIColor.greenColor()
         positiveView.alpha = 0
         
-        backgroundMinsView.addSubview(negattveView)
+        backgroundMinsView.addSubview(negativeView)
         backgroundMinsView.addSubview(positiveView)
         
         var positiveFrame : CGRect = positiveView.frame
         positiveFrame.size.width = 0
         
-        var negFrame = negattveView.frame
+        var negFrame = negativeView.frame
         negFrame.size.width = CGFloat(neg) * pxPrMinute
         negFrame.origin.x = 0
         
         positiveView.alpha = 1
-        negattveView.alpha = 1
+        negativeView.alpha = 1
         
         UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
                 positiveView.frame = positiveFrame
             }, completion: {finished in
                 UIView.animateWithDuration(2, animations: {
-                    negattveView.frame = negFrame
+                    negativeView.frame = negFrame
                 })
         })
+        
+        //set the end minutes
+        if let maxMins = activityGoal.maxDurationMinutes {
+            self.endMinutes.text = String(maxMins)
+        }
+        
+        //set beyond goal label minutes
+        if neg == 0 {
+            self.minutesBeyondGoal.hidden = true
+        }
+        self.minutesBeyondGoal.text = String(neg)
+        
+        //set position of the zero point
+        let zeroMinsFrametemp = self.endMinutes.frame
+        let zeroMins = UILabel(frame: CGRectMake(CGFloat(neg) * pxPrMinute, zeroMinsFrametemp.origin.y, zeroMinsFrametemp.size.width, zeroMinsFrametemp.size.height))
+        zeroMins.text = "0"
+        zeroMins.font = UIFont(name: "SFUIDisplay-Regular", size: 11)
+        zeroMins.textColor = UIColor.yiBlackColor()
+        zeroMins.alpha = 0.5
+        self.addSubview(zeroMins)
+        
+        //set minutes title
+        if neg != 0 {
+            self.minutesTitle.textColor = UIColor.yiDarkishPinkColor()
+            self.minutesTitle.text = String(neg)
+            self.goalMessage.text = NSLocalizedString("meday.message.minutesover", comment: "")
+        } else {
+            self.minutesTitle.text = String(positive)
+            self.goalMessage.text = NSLocalizedString("meday.message.minutesleft", comment: "")
+        }
+        
+        //set goal title
+        self.goalType.text = activityGoal.goalType
+        
     }
 
 }
