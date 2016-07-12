e8b14de91f1914c4320a8bcc8df2a3d037151772
Ben Smith <benjaminsmith1981@gmail.com>
2016-07-12 18:04:25 +0200

    no go view

---
+++ Yona/Yona/UIControlCell/NoGoView.swift
@@ -0,0 +1,19 @@
+//
+//  NoGoView.swift
+//  Yona
+//
+//  Created by Ben Smith on 12/07/16.
+//  Copyright © 2016 Yona. All rights reserved.
+//
+
+import Foundation
+
+class NoGoView : UITableViewCell {
+    
+    @IBOutlet weak var nogoImage: UIImageView!
+    @IBOutlet weak var nogoType: UILabel!
+    @IBOutlet weak var nogoMessage: UILabel!
+    
+    override func awakeFromNib() {
+    }
+}
