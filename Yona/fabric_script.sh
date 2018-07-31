8d503608e4fd8a3f999e732f84ed9315cd79b95c
Vishal Revdiwala <vishal.r@comakeit.com>
2018-07-31 19:42:07 +0530

    testing logs in build phase script

--- Yona/fabric_script.sh
+++ Yona/fabric_script.sh
@@ -1,22 +1,3 @@
-FABRIC_APIKEY_FILE="$FABRIC_API_KEY"
-FABRIC_BUILDSECRET_FILE="$FABRIC_BUILD_SECRET"
+echo 'yona build test cmi 1'
+./Pods/Fabric/run "$FABRIC_API_KEY" "$FABRIC_BUILD_SECRET"
 
-if test ! -f "$FABRIC_APIKEY_FILE" -o ! -f "$FABRIC_BUILDSECRET_FILE"; then
-# Let the build fail
-exit 1
-fi
-
-FABRIC_APIKEY=$(cat "$FABRIC_APIKEY_FILE")
-if test $? -ne 0; then
-echo "Cannot read $FABRIC_APIKEY_FILE"
-exit 1
-fi
-
-FABRIC_BUILDSECRET=$(cat "$FABRIC_BUILDSECRET_FILE")
-if test $? -ne 0; then
-echo "Cannot read $FABRIC_BUILDSECRET_FILE"
-exit 1
-fi
-
-echo "Uploading dSYM files to Crashlytics"
-"${PODS_ROOT}/Fabric/run" "$FABRIC_APIKEY" "$FABRIC_BUILDSECRET"
